10a825afdfd95a7ab527ad1e4ccf05aba148a1de
KHALED AHMMED ANIK <59437962+Khaled227@users.noreply.github.com>
2022-06-10 04:32:35 +0600

    fixed float button works

--- src/main/java/com/khaledahmmedanik/main/repository/StudentRepository.java
+++ src/main/java/com/khaledahmmedanik/main/repository/StudentRepository.java
@@ -16,16 +16,9 @@ public interface StudentRepository extends MongoRepository<Student, String>{
 	 @Query("{id :?0}")                                                  //SQL Equivalent : SELECT * FROM BOOK WHERE ID=?
      Student getStudentById(String id);
 
-		/*
-		 * @Query(value = "{entryTime:?0}", sort= "{name:1}") List<Student>
-		 * getBooksByAuthorSortByName(String entryTime);
-		 */
-		/*
-		 * @Query("{ 'entryTime' : ?0}") List<Student>
-		 * findAllStudentInEntryTimeSrotedOrder(String entryTime, Sort sort);
-		 */
 	 
 	 @Query(value ="{exitTime: ?0}", count=true)
      Integer getTotalBookedSeatNumber(String exitTime);
-	
+
+
 }
